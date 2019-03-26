1708f0f1f04f78fbb64d5ecade8a4effb3ae305a
quanghuy10796 <nguyenquanghuy10796@gmail.com>
2019-03-26 16:33:04 +0700

    Add a full_title helper

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
