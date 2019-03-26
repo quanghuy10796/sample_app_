75faaf6b57c8133e7b35d6af9f0b2893a5192c1c
quanghuy10796 <nguyenquanghuy10796@gmail.com>
2019-03-26 15:55:59 +0700

    vd

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/about'
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
